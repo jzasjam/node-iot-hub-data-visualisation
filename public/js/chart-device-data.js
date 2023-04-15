7356536fc9737c531e6725052b03d5d1615611d0
jzasjam <jonathan.zasada-james@student.shu.ac.uk>
2023-04-15 14:45:50 +0100

    Test fix ws

--- public/js/chart-device-data.js
+++ public/js/chart-device-data.js
@@ -4,7 +4,7 @@
 $(document).ready(() => {
   // if deployed to a site supporting SSL, use wss://
   const protocol = document.location.protocol.startsWith('https') ? 'wss://' : 'ws://';
-  const webSocket = new WebSocket(protocol + location.host);
+  const webSocket = new WebSocket(protocol + location.host + '/public');
 
   // A class for holding the last N points of telemetry for a device
   class DeviceData {
