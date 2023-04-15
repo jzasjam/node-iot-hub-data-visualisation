320a9db4ad0a065222320cb63bcf2fc3b46b518b
jzasjam <jonathan.zasada-james@student.shu.ac.uk>
2023-04-15 15:22:14 +0100

    Undo change to ws

--- public/js/chart-device-data.js
+++ public/js/chart-device-data.js
@@ -4,7 +4,7 @@
 $(document).ready(() => {
   // if deployed to a site supporting SSL, use wss://
   const protocol = document.location.protocol.startsWith('https') ? 'wss://' : 'ws://';
-  const webSocket = new WebSocket(protocol + location.host + '/public');
+  const webSocket = new WebSocket(protocol + location.host);
 
   // A class for holding the last N points of telemetry for a device
   class DeviceData {
